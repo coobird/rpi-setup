28f99dbac59cdbe002ca780692c545f106c36e83
Chris Kroells <coobird@coobird.net>
2015-06-14 20:57:50 +0900

    Improved SSH configuration script.
    First, check that the changes to the configuration sticks and that
    it is correct. Then, SSH daemon is restarted to apply the configuration
    file.

--- bin/config-ssh.sh
+++ bin/config-ssh.sh
@@ -2,4 +2,13 @@
 
 # Disable passing of environment variables via SSH.
 sed -i -e 's/^AcceptEnv/#AcceptEnv/' /etc/ssh/sshd_config
+test $? -ne 0 && echo "*** Failed to change sshd_config file." && exit 1
+
+sshd -t
+if [[ $? -eq 0 ]]; then
+  /etc/init.d/ssh reload
+else
+  echo "*** Stopped. Incorrect configuration for sshd."
+  exit 1
+fi
 
